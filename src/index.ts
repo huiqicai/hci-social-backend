06708e63923fb9ff20bc431eff055d58c011bf6f
Jonathan Romano <jonathan@luxaritas.com>
2021-02-23 15:14:26 -0500

    fcgi support

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import 'source-map-support/register';
 
-// std
-import * as http from 'http';
-
 // 3p
 import { Config, createApp } from '@foal/core';
 
@@ -12,14 +9,17 @@ import { AppController } from './app/controllers/app.controller';
 async function main() {
   const app = await createApp(AppController);
 
-  const httpServer = http.createServer(app);
   const socket = Config.get('listen_socket', 'string');
   if (socket) {
+    const fcgi = require('node-fastcgi');
+    const httpServer = fcgi.createServer(app);
     httpServer.listen(socket, () => {
       console.log(`Listening on socket ${socket}...`);
     });
   } else {
     const port = Config.get('port', 'number', 3001);
+    const http = require('http');
+    const httpServer = http.createServer(app);
     httpServer.listen(port, () => {
       console.log(`Listening on port ${port}...`);
     });
