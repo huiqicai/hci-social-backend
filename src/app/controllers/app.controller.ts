ce8e61cc8024ee0d2f7449b0bf690ea7eff212e9
Jonathan Romano <jonathan@luxaritas.com>
2021-02-25 02:08:31 -0500

    API prefix for apache schenanigans

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -1,13 +1,14 @@
-import { controller, IAppController } from '@foal/core';
+import { Config, controller, IAppController } from '@foal/core';
 import { createConnection } from 'typeorm';
 
 import { ApiController } from './api.controller';
 import { OpenApiController } from './openapi.controller';
 
+const prefix = Config.get('api_prefix', 'string', '');
 export class AppController implements IAppController {
   subControllers = [
-    controller('/swagger', OpenApiController),
-    controller('/api', ApiController),
+    controller(`${prefix}/swagger`, OpenApiController),
+    controller(`${prefix}/api`, ApiController),
   ];
 
   async init() {
