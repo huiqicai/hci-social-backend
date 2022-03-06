a671c24d2d13c9b032e0485145df01313cca1d0b
Jonathan Romano <jonathan@luxaritas.com>
2022-03-06 16:39:42 -0500

    Fix parsing/validation of multiple attributes filters

--- src/app/hooks/parse-attributes.hook.ts
+++ src/app/hooks/parse-attributes.hook.ts
@@ -1,13 +1,23 @@
-import { Context, Hook, HookDecorator, ServiceManager } from '@foal/core';
+import { Context, Hook, HookDecorator, HttpResponseBadRequest, ServiceManager } from '@foal/core';
+
+function isStringArray(arr: unknown[]): arr is string[] {
+  return !arr.some(entry => typeof entry !== 'string');
+}
 
 export function ParseAttributes(): HookDecorator {
   return Hook(async (ctx: Context, services: ServiceManager) => {
     const query: {attributes?: string | unknown} = ctx.request.query as {attributes?: string | unknown};
 
-    if (query.attributes && typeof query.attributes === 'string') {
+    if (!query.attributes) return;
+
+    if (typeof query.attributes === 'string') {
       const parsed = JSON.parse(query.attributes) as unknown;
-      if (Array.isArray(parsed)) query.attributes = parsed;
-      else query.attributes = [parsed];
+      query.attributes = [parsed];
+    } else if (Array.isArray(query.attributes) && isStringArray(query.attributes)) {
+      const parsed = query.attributes.map(filter => JSON.parse(filter) as unknown);
+      query.attributes = parsed;
+    } else {
+      return new HttpResponseBadRequest('Attributes filter should be either a JSON string or an array of JSON strings');
     }
   });
 }
